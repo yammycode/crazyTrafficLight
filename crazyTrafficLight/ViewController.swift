81ea194f5eac7491054e69883e7f52215381de83
Anton <info@yammycode.com>
2022-07-29 08:05:25 +0300

    Initial Commit

---
+++ crazyTrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  crazyTrafficLight
+//
+//  Created by Anton Saltykov on 29.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
