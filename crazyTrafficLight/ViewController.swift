0496c6981da1ddfd952f536d9c80f3b29bca7abe
Anton <info@yammycode.com>
2022-07-29 20:23:52 +0300

    Реализована логика светофора

--- crazyTrafficLight/ViewController.swift
+++ crazyTrafficLight/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var lightButton: UIButton!
+    
+    
+    var opacity = 0.3
+    var lights: [UIView] = []
+    var activeLightIndex: Int?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        lightButton.backgroundColor = .systemBlue
+        lightButton.setTitle("START", for: .normal)
+        lightButton.layer.cornerRadius = 20
+        lightButton.titleLabel?.font = UIFont.systemFont(ofSize: 24)
+        lightButton.tintColor = .white
+        
+        lights = [redLightView, yellowLightView, greenLightView]
+        
+        for light in lights {
+            light.layer.cornerRadius = light.frame.size.width / 2
+            light.alpha = opacity
+        }
+        
     }
-
+    
+    @IBAction func changeLight() {
+        
+        guard !lights.isEmpty else { return }
+        
+        lightButton.setTitle("NEXT", for: .normal)
+        
+        if var index = activeLightIndex {
+            lights[index].alpha = opacity
+            index = index == lights.count - 1 ? 0 : index + 1
+            lights[index].alpha = 1
+            activeLightIndex = index
+        } else {
+            lights[0].alpha = 1
+            activeLightIndex = 0
+        }
+    }
 
 }
 
